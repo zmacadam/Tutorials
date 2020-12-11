e37868acdc808718e256997322ccd2e7a1071f2f
Zach Macadam <z.macadam@tcu.edu>
2020-12-11 14:57:49 -0800

    Added Spring Boot and Kotlin tutorial.
    Tutorial completed to practice Spring Boot and gain an introduction to Kotlin and Mustache.
    
    https://spring.io/guides/tutorials/spring-boot-kotlin/

---
+++ SpringBootAndKotlin/src/test/kotlin/zmacadam/springpractice/springbootandkotlin/SpringbootandkotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package zmacadam.springpractice.springbootandkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootandkotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
